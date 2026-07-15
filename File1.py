452be1501e9831687c99a628eb19b025673c46e0
imnarang <monunarang52@gmail.com>
2026-07-15 11:13:50 +0530

    Add Python code validation

--- File1.py
+++ File1.py
@@ -1,2 +1,13 @@
 import pandas
 import os
+from pathlib import Path
+
+folder_path = Path(r"D:\YourFolder")
+
+files = [
+    file_path.name
+    for file_path in folder_path.iterdir()
+    if file_path.is_file()
+]
+
+print(files)
